43239977028087990cf6525dfd355ab4b43fbb73
franyelica <franyelicavalentina.fvgf@gmail.com>
2022-03-04 21:46:21 -0500

    Examen practico commit

--- Examen Practico/Ejercicio Bonus/Mayusculas.js
+++ Examen Practico/Ejercicio Bonus/Mayusculas.js
@@ -1,4 +1,12 @@
-
-  var  cadena= 'www.examen.com'
-  cadena= cadena.toUpperCase();
-  console.log(cadena);
+function toUpperCase(word,index){
+  if(index +1){
+   return word.substring(0,index).toUpperCase()+word.substring(index)
+  }
+  return word;
+}
+ function main(){
+  const word = "www.mayuscula.com";
+  console.log('Antes:',word);
+  console.log('Despues:',toUpperCase(word,word.indexOf('.')));
+ }
+ main()
